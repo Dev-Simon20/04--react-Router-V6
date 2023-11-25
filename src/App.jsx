3ff8b7df7b9f675a732c7b321bed2bd69043adae
Dev-Simon20 <jeanpierks6@gmail.com>
2023-11-25 16:35:04 -0500

    Renderiza el header y enruta los componenets Home,Page,Contacto

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { BrowserRouter, Routes, Route,Link, Navigate } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Acerca from "./pages/Acerca";
+import Contacto from "./pages/Contacto";
+import Header from "./components/Header";
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [login,setLogin]=useState(false)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header></Header>
+      
+      <BrowserRouter>
+       <nav className="menu">
+        <Link to="/">Home</Link>
+        <Link to="/acerca">Acerca</Link>
+        <Link to="/contacto">Contacto</Link>
+       </nav>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/acerca" element={<Acerca/>}/>
+          <Route path="/contacto" element={<Contacto/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Acerca.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react"
+
+const Acerca=()=>{
+ 
+    return(
+        <>
+        <h1>Acerca</h1>
+        </>
+    )
+}
+export default Acerca
